bba18d7d63b97c12c9c1c4f7fa55ff90399f74d5
jasond13123@gmail.com <jasond13123@gmail.com>
2022-04-23 17:18:58 -0400

    correct substract

--- tests/src/test_calculator.py
+++ tests/src/test_calculator.py
@@ -2,11 +2,11 @@ from app.src.calculator import Calculator
 
 cal = Calculator()
 
-def test__add():
+def test_add():
     assert cal.add(1, 1) == 2
     assert cal.add(1, 2) == 3
 
 def test_substract():
-    assert cal.substract(3, 1) == 2
-    assert cal.substract(2,1) == 1
+    assert cal.substract(5, 2) == 3
+    assert cal.substract(2, 1) == 1
 
